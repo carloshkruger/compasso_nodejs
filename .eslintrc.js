56d2da3f1c78d800a942f8842999457a2f327cab
Carlos H Kruger <carloshkruger@gmail.com>
2021-05-22 16:49:38 -0300

    added and configured eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['prettier', 'airbnb-base'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    semi: 'off',
+  },
+}
