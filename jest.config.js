10225aa155446a44e88d9aabcf382da886f663b5
Carlos H Kruger <carloshkruger@gmail.com>
2021-05-22 22:16:58 -0300

    created the test coverage config and script

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,13 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/repositories/**'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 }
