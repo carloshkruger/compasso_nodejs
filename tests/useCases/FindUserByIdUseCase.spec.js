406d52021cd80daf4e0f3687072ccba3a567a33c
Carlos H Kruger <carloshkruger@gmail.com>
2021-05-23 08:55:47 -0300

    created the FindUserById use case and tests

---
+++ tests/useCases/FindUserByIdUseCase.spec.js
@@ -0,0 +1,44 @@
+const FindUserByIdUseCase = require('../../src/useCases/FindUserByIdUseCase')
+const FakeUsersRepository = require('../../src/infra/repositories/inMemory/FakeUsersRepository')
+const AppError = require('../../src/core/AppError')
+const UniqueId = require('../../src/core/UniqueId')
+const User = require('../../src/domain/User')
+
+let fakeUsersRepository
+let findUserByIdUseCase
+
+describe('FindUserByIdUseCase', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository()
+    findUserByIdUseCase = new FindUserByIdUseCase({
+      usersRepository: fakeUsersRepository
+    })
+  })
+
+  test('should throw if user is is not provided', async () => {
+    await expect(findUserByIdUseCase.execute({ userId: '' })).rejects.toThrow(AppError)
+  })
+
+  test('should throw if user not found', async () => {
+    jest.spyOn(fakeUsersRepository, 'findById').mockImplementationOnce(async () => undefined)
+
+    const userId = new UniqueId().value
+
+    await expect(findUserByIdUseCase.execute({ userId })).rejects.toThrow(AppError)
+  })
+
+  test('should return an user if exists', async () => {
+    const user = new User({
+      name: 'Valid user name',
+      gender: 'valid gender',
+      birthdate: new Date(),
+      cityId: new UniqueId().value,
+    })
+
+    jest.spyOn(fakeUsersRepository, 'findById').mockImplementationOnce(async () => user)
+
+    const useCaseResponse = await findUserByIdUseCase.execute({ userId: user.id })
+    
+    expect(useCaseResponse).toBeInstanceOf(User)
+  })
+})
