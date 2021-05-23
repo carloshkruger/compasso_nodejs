c4475e546c744ecabc09129ad3e3b3401cb095a9
Carlos H Kruger <carloshkruger@gmail.com>
2021-05-23 17:19:41 -0300

    disabled sequelize logs

--- src/infra/sequelize/utils/createInstance.js
+++ src/infra/sequelize/utils/createInstance.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize')
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'src/infra/sequelize/database.sqlite',
+  logging: false,
 })
 
 module.exports = sequelize
