12a7b0a18f8121610f662f64bc150ffeb018073e
Carlos H Kruger <carloshkruger@gmail.com>
2021-05-23 12:22:26 -0300

    changed the jest config file

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,17 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js', '!**/src/repositories/**'],
+  collectCoverageFrom: [
+    '**/src/**/*.js',
+    '!**/src/repositories/**',
+    '!**/src/infra/repositories/inMemory/**',
+  ],
   coverageThreshold: {
     global: {
       branches: 100,
       functions: 100,
       lines: 100,
-      statements: 100
-    }
-  }
+      statements: 100,
+    },
+  },
 }
