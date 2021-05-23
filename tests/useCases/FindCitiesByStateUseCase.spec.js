468f038f542351f0e9600298b3db5558588b563c
Carlos H Kruger <carloshkruger@gmail.com>
2021-05-23 12:13:45 -0300

    apply eslint on FindCitiesByStateUseCase.spec.js

--- tests/useCases/FindCitiesByStateUseCase.spec.js
+++ tests/useCases/FindCitiesByStateUseCase.spec.js
@@ -1,8 +1,8 @@
-const AppError = require("../../src/core/AppError")
-const FieldRequiredError = require("../../src/core/FieldRequiredError")
-const City = require("../../src/domain/City")
-const FakeCitiesRepository = require("../../src/infra/repositories/inMemory/FakeCitiesRepository")
-const FindCitiesByStateUseCase = require("../../src/useCases/FindCitiesByStateUseCase")
+const AppError = require('../../src/core/AppError')
+const FieldRequiredError = require('../../src/core/FieldRequiredError')
+const City = require('../../src/domain/City')
+const FakeCitiesRepository = require('../../src/infra/repositories/inMemory/FakeCitiesRepository')
+const FindCitiesByStateUseCase = require('../../src/useCases/FindCitiesByStateUseCase')
 
 let fakeCitiesRepository
 let findCitiesByStateUseCase
@@ -11,18 +11,24 @@ describe('FindCitiesByStateUseCase', () => {
   beforeEach(() => {
     fakeCitiesRepository = new FakeCitiesRepository()
     findCitiesByStateUseCase = new FindCitiesByStateUseCase({
-      citiesRepository: fakeCitiesRepository
+      citiesRepository: fakeCitiesRepository,
     })
   })
 
   test('should throw if state was not provided', async () => {
-    await expect(findCitiesByStateUseCase.execute({state: ''})).rejects.toThrow(FieldRequiredError)
+    await expect(
+      findCitiesByStateUseCase.execute({ state: '' }),
+    ).rejects.toThrow(FieldRequiredError)
   })
 
   test('should return an empty list if no city was found', async () => {
-    jest.spyOn(fakeCitiesRepository, 'findByState').mockImplementationOnce(async () => [])
+    jest
+      .spyOn(fakeCitiesRepository, 'findByState')
+      .mockImplementationOnce(async () => [])
 
-    const useCaseResponse = await findCitiesByStateUseCase.execute({state: 'SC'})
+    const useCaseResponse = await findCitiesByStateUseCase.execute({
+      state: 'SC',
+    })
 
     expect(useCaseResponse.length).toBe(0)
   })
@@ -30,17 +36,21 @@ describe('FindCitiesByStateUseCase', () => {
   test('should return a list of cities', async () => {
     const city1 = new City({
       name: 'Aurora',
-      state: 'SC'
+      state: 'SC',
     })
 
     const city2 = new City({
       name: 'Rio do Sul',
-      state: 'SC'
+      state: 'SC',
     })
 
-    jest.spyOn(fakeCitiesRepository, 'findByState').mockImplementationOnce(async () => [city1, city2])
+    jest
+      .spyOn(fakeCitiesRepository, 'findByState')
+      .mockImplementationOnce(async () => [city1, city2])
 
-    const useCaseResponse = await findCitiesByStateUseCase.execute({state: 'SC'})
+    const useCaseResponse = await findCitiesByStateUseCase.execute({
+      state: 'SC',
+    })
 
     expect(useCaseResponse.length).toBe(2)
   })
